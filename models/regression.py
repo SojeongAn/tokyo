b53aa378a032fa344721de9193d5feda3307941c
Sojung An <39581678+SojeongAn@users.noreply.github.com>
2022-01-16 16:04:33 +0900

    Create regression.py

---
+++ models/regression.py
@@ -0,0 +1,12 @@
+import torch
+
+class regression(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
+        self.predict = torch.nn.Linear(n_hidden, n_output)   # output layer
+
+    def forward(self, x):
+        x = F.relu(self.hidden(x))      # activation function for hidden layer
+        x = self.predict(x)             # linear output
+        return x
